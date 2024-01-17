c3823cba7f9effdabcc41c25d18a6a92b03824f0
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2024-01-17 14:05:04 +0600

    change password

--- Backend/routes/api/authroutes.js
+++ Backend/routes/api/authroutes.js
@@ -4,11 +4,12 @@ const registrationController = require("../../controllers/registrationController
 const otpController = require("../../controllers/otpController");
 const loginController = require("../../controllers/loginController");
 const forgotpasswordController = require("../../controllers/forgotpassword");
-
+const changepasswordController = require("../../controllers/changepasswordController");
 
 router.post("/registration", registrationController);
-router.post("/otpverify", otpController)
-router.post("/login",loginController)
-router.post("/forgotpassword", forgotpasswordController )
+router.post("/otpverify", otpController);
+router.post("/login", loginController);
+router.post("/forgotpassword", forgotpasswordController);
+router.post("/changepassword", changepasswordController);
 
 module.exports = router;
